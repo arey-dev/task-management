e7560bd46ecd6d0ea7915a64a5c6151d7aa864d5
R.A Borja <raborja.dev@gmail.com>
2023-07-09 03:14:11 +0800

    replaced by BoardLayout

--- src/components/Layout.jsx
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line react/prop-types
-export function Layout({ children }) {
-  return (
-    <div className="grid grid-cols-2 grid-rows-2 max-w-[1440px] min-h-screen">
-      {children}
-    </div>
-  );
-}
