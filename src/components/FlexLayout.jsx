a2caa79ea973001c65068dfa493bc8586931fea0
R.A Borja <raborja.dev@gmail.com>
2023-07-12 04:42:47 +0800

    add center prop

--- src/components/FlexLayout.jsx
+++ src/components/FlexLayout.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable react/prop-types */
 import { twJoin } from "tailwind-merge";
 
-export function FlexLayout({ children, className, as = "div" }) {
+export function FlexLayout({ children, center, as = "div", className }) {
   const Component = as;
   return (
-    <Component className={twJoin("flex max-w-[1440px] mx-auto", className)}>
+    <Component
+      className={twJoin(
+        "flex max-w-[1440px] mx-auto",
+        center && "items-center justify-center",
+        className
+      )}
+    >
       {children}
     </Component>
   );
