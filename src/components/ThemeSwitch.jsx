97b9b9fd687ad087d33ce871f439c882239b5829
R.A Borja <raborja.dev@gmail.com>
2023-07-13 15:49:00 +0800

    fix className issues

--- src/components/ThemeSwitch.jsx
+++ src/components/ThemeSwitch.jsx
@@ -10,7 +10,7 @@ export function ThemeSwitch() {
     >
       <img src={light} alt="light theme icon" />
       <label className="relative inline-flex items-center cursor-pointer">
-        <input type="checkbox" value="" class="sr-only peer" />
+        <input type="checkbox" value="" className="sr-only peer" />
         <div className="w-11 h-6 bg-primary peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-on-primary after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
         <span className="sr-only">Theme Switch</span>
       </label>
