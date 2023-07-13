fa38f2313389c50726eeb8d1500e31e751fb51a3
R.A Borja <raborja.dev@gmail.com>
2023-07-13 17:23:33 +0800

    remove border guide

--- src/routes/Board.jsx
+++ src/routes/Board.jsx
@@ -7,7 +7,7 @@ export function Board() {
   const { columns } = useOutletContext();
 
   return (
-    <Flex as="main" className="border border-l-primary w-full p-6 gap-6 overflow-auto">
+    <Flex as="main" className="w-full p-6 gap-6 overflow-auto">
       {columns.map((column) => (
         <Column
           key={column.name}
