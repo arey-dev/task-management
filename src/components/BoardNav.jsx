55c2b4dac9aa5e141b39d2363dac8d23ab0083ac
R.A Borja <raborja.dev@gmail.com>
2023-07-13 17:21:59 +0800

    create HideButton for BoardNav

--- src/components/BoardNav.jsx
+++ src/components/BoardNav.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
 import { ThemeSwitch } from "./ThemeSwitch";
 import { Link } from "./ui/Link";
+import { HideButton } from "./HideButton";
 
 export function BoardNav({ boards }) {
   return (
-    <nav className="min-w-[18.75rem] pt-4 border-x border-light-lines bg-light-surface">
+    <nav className="min-w-[18.75rem] pt-4 pb-8 border-x border-light-lines bg-light-surface flex flex-col">
       <h2 className="text-heading-sm text-on-background ml-8 mb-[1.125rem]">
         All BOARDS ({boards.length})
       </h2>
@@ -22,7 +23,10 @@ export function BoardNav({ boards }) {
           />
         </li>
       </ul>
-      <ThemeSwitch />
+      <div className="mt-auto flex flex-col gap-2">
+        <ThemeSwitch />
+        <HideButton className="relative right-6" />
+      </div>
     </nav>
   );
 }
---
+++ src/components/HideButton.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import { twJoin } from "tailwind-merge";
+import hide from "../assets/icon-hide-sidebar.svg";
+
+export function HideButton({ className }) {
+  return (
+    <button
+      className={twJoin(
+        "block text-on-background rounded-full transition ease-in-out duration-300 text-heading-md text-start pl-[3.25rem] py-[0.875rem] focus:outline-none focus:ring-1 focus:ring-primary hover:bg-light-secondary hover:text-on-secondary",
+        className
+      )}
+    >
+      <img
+        className="inline mr-4 align-text-top"
+        src={hide}
+        alt="Hide Sidebar Icon"
+      />
+      Hide Sidebar
+    </button>
+  );
+}
