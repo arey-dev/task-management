06025d0c11ccb5702f0e3ee5016348030b6bf4f0
R.A Borja <raborja.dev@gmail.com>
2023-07-14 19:14:57 +0800

    add modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+
+export function Modal({ children }) {
+  return (
+    <div className="fixed left-0 top-0 right-0 bottom-0 bg-[#000000] bg-opacity-60 flex items-center justify-center">
+      {children}
+    </div>
+  );
+}
