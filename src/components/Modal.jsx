097a2f179ee9ff69e8c6fbd91370670d1dbf93a6
R.A Borja <raborja.dev@gmail.com>
2023-07-16 05:04:29 +0800

    add overflow-auto

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,7 @@
 
 export function Modal({ children }) {
   return (
-    <div className="fixed left-0 top-0 right-0 bottom-0 bg-[#000000] bg-opacity-60 flex items-center justify-center">
+    <div className="fixed left-0 top-0 right-0 bottom-0 bg-[#000000] bg-opacity-60 flex items-center justify-center overflow-auto">
       {children}
     </div>
   );
