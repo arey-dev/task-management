8b61d8b58c0091f77d67538466d7284fe65af41b
R.A Borja <raborja.dev@gmail.com>
2023-07-17 16:44:47 +0800

    move state to AddTask component

--- src/components/form/Dropdown.jsx
+++ src/components/form/Dropdown.jsx
@@ -1,43 +1,32 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import chevronDown from "../../assets/icon-chevron-down.svg";
 import { Input } from "./Input";
 
-const initialOptions = [
-  { id: 0, value: "Todo" },
-  { id: 1, value: "Doing" },
-  { id: 2, value: "Done" },
-  { id: 4, value: "later" },
-  { id: 5, value: "tomorrow" },
-];
-
-export function Dropdown() {
-  const [option, setOption] = useState(initialOptions[0]);
+export function Dropdown({ options, selectedOption, onOptionChange }) {
   const [isOpen, setIsOpen] = useState(false);
-
-  const handleOptionChange = (option) => {
-    setOption(option);
-    setIsOpen(false);
-  };
 
   return (
     <div className="relative">
-      <Input label="Status" value={option.value} hidden />
-
+      <Input label="Status" defaultValue={selectedOption.value} hidden />
       <button
         type="button"
         onClick={() => setIsOpen(!isOpen)}
         className="flex py-2 px-4 justify-between items-center w-full border text-body-lg border-neutral-1 rounded-md outline-primary"
       >
-        {option.value}
+        {selectedOption.value}
         <img src={chevronDown} alt="Chevron Down Icon" />
       </button>
       {isOpen && (
         <ul className="absolute left-0 top-full mt-3 dropdown-shadow flex flex-col gap-2 w-full bg-light-surface text-on-background text-body-lg p-4 rounded-md">
-          {initialOptions.map((option) => (
+          {options.map((option) => (
             <li
-              className="hover:cursor-pointer"
+              className="hover:text-primary hover:cursor-pointer"
               key={`${option.id}${option.value}`}
-              onClick={() => handleOptionChange(option)}
+              onClick={() => {
+                onOptionChange(option);
+                setIsOpen(false)
+              }}
             >
               {option.value}
             </li>
