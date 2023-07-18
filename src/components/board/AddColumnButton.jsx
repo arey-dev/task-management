068ad338f6606914c3d0aca07bea88cd0ef148ce
R.A Borja <raborja.dev@gmail.com>
2023-07-18 18:22:05 +0800

    lower z-index value

--- src/components/board/AddColumnButton.jsx
+++ src/components/board/AddColumnButton.jsx
@@ -1,6 +1,6 @@
 export function AddColumnButton() {
   return (
-    <button className="w-[17.5rem] max-h-[53.3125rem] relative top-[2.45rem] bg-add-column text-on-background text-2xl rounded-md shrink-0 hover:text-primary transition">
+    <button className="w-[17.5rem] max-h-[53.3125rem] relative top-[2.45rem] bg-add-column text-on-background text-2xl rounded-md shrink-0 hover:text-primary transition z-40">
       + New Column
     </button>
   );
