4be1c95f03335bb241437b120e80d80fced3755c
R.A Borja <raborja.dev@gmail.com>
2023-07-18 17:02:22 +0800

    display board data bases on path

--- src/routes/Board.jsx
+++ src/routes/Board.jsx
@@ -1,15 +1,25 @@
-import { Outlet, useOutletContext } from "react-router-dom";
+import { Outlet, useOutletContext, useParams } from "react-router-dom";
 import { Flex } from "../components/Flex";
 // import { CallToAction } from "../components/CallToAction";
 import { Column } from "../components/board/Column";
 import { AddColumnButton } from "../components/board/AddColumnButton";
+import { removeDelimiter } from "../utilities";
 
 export function Board() {
-  const { columns } = useOutletContext();
+  // read data from parent component that is passed from Outlet
+  const boards = useOutletContext();
+
+  // get board name from params
+  const { boardId } = useParams();
+
+  // find the board that has the same name as the boardId from params
+  const [board] = boards.filter(
+    (board) => board.name === removeDelimiter(boardId, "-")
+  );
 
   return (
     <Flex as="main" className="w-full px-6 pt-6 gap-6 overflow-auto">
-      {columns.map((column) => (
+      {board?.columns.map((column) => (
         <Column
           key={column.name}
           columnName={column.name}
