2e8ecb5598f3535d68cc23038416ce32b40f2989
R.A Borja <raborja.dev@gmail.com>
2023-07-18 18:24:14 +0800

    only pass boards data to Outlet and BoardNav component

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -2,8 +2,8 @@ import { Flex } from "../components/Flex";
 import { Logo } from "../components/Logo";
 import { AppBar } from "../components/AppBar";
 import { BoardNav } from "../components/BoardNav";
-import data from "../data.json";
 import { Outlet } from "react-router-dom";
+import data from "../data.json";
 
 export function Root() {
   const { boards } = data;
@@ -12,11 +12,11 @@ export function Root() {
     <>
       <Flex className="h-24">
         <Logo />
-        <AppBar boardTitle={boards[0].name} />
+        <AppBar />
       </Flex>
       <Flex className="h-[calc(100vh-6rem)]">
         <BoardNav boards={boards} />
-        <Outlet context={boards[0]} />
+        <Outlet context={boards} />
       </Flex>
     </>
   );
