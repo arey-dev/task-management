ca273df59ed3f3f6837e2a71065fc5aa3d64055d
R.A Borja <raborja.dev@gmail.com>
2023-07-18 16:16:54 +0800

    create helper function to parse board name

---
+++ src/utilities/index.js
@@ -0,0 +1,9 @@
+export function hypenateString(string) {
+  // Replace spaces with hyphens using the regular expression
+  return string.replace(/\s+/g, "-");
+}
+
+export function removeDelimiter(string, delimiter) {
+  // Split the string at each delimiter and then join with spaces
+  return string.split(delimiter).join();
+}
