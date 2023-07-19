e8a29ba8d15209afe93da2bb2860f3fa8c6678ae
R.A Borja <raborja.dev@gmail.com>
2023-07-19 19:23:12 +0800

    create handler for navigating to task-view

--- src/components/board/Task.jsx
+++ src/components/board/Task.jsx
@@ -1,15 +1,27 @@
 /* eslint-disable react/prop-types */
+import { useNavigate, useParams } from "react-router-dom";
 
-export function Task({ task }) {
-  const { title, subtasks } = task;
+export function Task({ task, column }) {
+  const { taskId, title, subtasks } = task;
+
   const subtaskCompleted = subtasks.filter(
     (subtask) => subtask.isCompleted
   ).length;
+
   const subtaskCount = subtasks.length;
 
+  const navigate = useNavigate();
+
+  const { boardId } = useParams();
+
   return (
-    <section className="bg-light-surface rounded-lg task-shadow px-4 py-6 last-of-type:last:mb-12">
-      <h3 className="text-heading-md text-light-on-surface mb-2">{title}</h3>
+    <section
+      onClick={() =>
+        navigate(`/board/${boardId}/${column}/task/${taskId}`)
+      }
+      className="bg-light-surface rounded-lg task-shadow px-4 py-6 last-of-type:last:mb-12 cursor-pointer text-light-on-surface hover:text-primary"
+    >
+      <h3 className="text-heading-md mb-2">{title}</h3>
       <p className="text-body-md text-on-background">
         <span>{subtaskCompleted}</span> of <span>{subtaskCount}</span> subtasks
       </p>
