03889cd4fcc6968136ff1bd800c71fcf9b12afec
R.A Borja <raborja.dev@gmail.com>
2023-07-19 13:47:35 +0800

    add logo to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ import { Flex } from "./Flex";
 import { HideButton } from "./HideButton";
 import { Modal } from "./Modal";
 import { ThemeSwitch } from "./ThemeSwitch";
+import { Logo } from "./Logo";
 import { CallToAction } from "./CallToAction";
 
-export { AppBar, BoardNav, Flex, HideButton, Modal, ThemeSwitch, CallToAction };
+export { AppBar, BoardNav, Flex, HideButton, Modal, ThemeSwitch, CallToAction, Logo };
