9fde41698618452705bc78ada3427b51502e9f24
R.A Borja <raborja.dev@gmail.com>
2023-07-19 15:24:19 +0800

    add checkbox component

---
+++ src/components/form/Checkbox.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+import { useId } from "react";
+import { twJoin } from "tailwind-merge";
+
+export function Checkbox({ checked, label, className, ...props }) {
+  const id = useId();
+
+  return (
+    <div
+      className={twJoin(
+        "flex items-center bg-light-background pl-3 pr-4 rounded gap-4",
+        className
+      )}
+    >
+      <input
+        checked={checked}
+        id={id}
+        type="checkbox"
+        value=""
+        name="bordered-checkbox"
+        className="w-4 h-4 text-primary bg-light-surface rounded-sm focus:ring-primary focus:outline-none focus:ring-1"
+        {...props}
+      />
+      <label
+        htmlFor={id}
+        className={twJoin(
+          "w-full py-4 text-body-md",
+          checked && "text-on-background line-through"
+        )}
+      >
+        {label}
+      </label>
+    </div>
+  );
+}
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,7 +1,8 @@
-import { Dropdown } from "./Dropdown";
-import { Input } from "./Input";
 import { Label } from "./Label";
+import { Input } from "./Input";
 import { RemovableInput } from "./RemovableInput";
+import { Checkbox } from "./Checkbox";
+import { Dropdown } from "./Dropdown";
 import { TextArea } from "./TextArea";
 
-export { Dropdown, Input, Label, RemovableInput, TextArea };
+export { Dropdown, Input, Label, RemovableInput, TextArea, Checkbox };
