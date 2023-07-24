cf6b629d840779c4f9b78ee4457720bc9bce5127
R.A Borja <raborja.dev@gmail.com>
2023-07-24 21:14:55 +0800

    install and configure firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "fem-task-management.firebaseapp.com",
+  projectId: "fem-task-management",
+  storageBucket: "fem-task-management.appspot.com",
+  messagingSenderId: "883356806951",
+  appId: "1:883356806951:web:60bd106cbf457b45f7c302",
+};
+
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
