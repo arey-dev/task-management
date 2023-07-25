c62c62c7068cbaec3a157535ef1279edcfc264aa
R.A Borja <raborja.dev@gmail.com>
2023-07-25 17:46:08 +0800

    configure hosting

--- src/App.jsx
+++ src/App.jsx
@@ -3,53 +3,50 @@ import { Root, Board, AddBoard, AddTask, TaskView, Welcome } from "./routes";
 import { removeDelimiter } from "./utilities";
 import data from "./data.json";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Root />,
-      // errorElement:
-      children: [
-        { index: true, element: <Welcome /> },
-        {
-          path: "/board/:boardId",
-          element: <Board />,
-          children: [
-            {
-              path: "add-task",
-              element: <AddTask />,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    // errorElement:
+    children: [
+      { index: true, element: <Welcome /> },
+      {
+        path: "/board/:boardId",
+        element: <Board />,
+        children: [
+          {
+            path: "add-task",
+            element: <AddTask />,
+          },
+          {
+            path: ":columnId/task/:taskId",
+            element: <TaskView />,
+            loader: async ({ params }) => {
+              const board = data.boards.find(
+                (board) => board.name === removeDelimiter(params.boardId, "-")
+              );
+
+              const column = board.columns.find(
+                (column) =>
+                  column.name === removeDelimiter(params.columnId, "-")
+              );
+
+              const task = column.tasks.find(
+                (task) => task.taskId === parseInt(params.taskId)
+              );
+
+              return { task };
             },
-            {
-              path: ":columnId/task/:taskId",
-              element: <TaskView />,
-              loader: async ({ params }) => {
-                const board = data.boards.find(
-                  (board) => board.name === removeDelimiter(params.boardId, "-")
-                );
-
-                const column = board.columns.find(
-                  (column) =>
-                    column.name === removeDelimiter(params.columnId, "-")
-                );
-
-                const task = column.tasks.find(
-                  (task) => task.taskId === parseInt(params.taskId)
-                );
-
-                return { task };
-              },
-            },
-          ],
-        },
-        {
-          path: "/board/add-board",
-          element: <AddBoard />,
-        },
-      ],
-    },
-  ],
-  { basename: "/task-management/" }
-);
+          },
+        ],
+      },
+      {
+        path: "/board/add-board",
+        element: <AddBoard />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
