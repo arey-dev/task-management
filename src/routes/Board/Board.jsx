1c22e9d18587b6cbfc1793418c9a9c7006496943
R.A Borja <raborja.dev@gmail.com>
2023-08-01 20:37:59 +0800

    render board tasks by status

--- src/routes/Board/Board.jsx
+++ src/routes/Board/Board.jsx
@@ -1,35 +1,29 @@
 import { Outlet, useLoaderData } from "react-router-dom";
 import { Flex } from "../../components";
-// import { CallToAction } from "../components/CallToAction";
+import { CallToAction } from "../../components";
 import { Column } from "../../components/board";
 import { AddColumnButton } from "../../components/board";
 
 export function Board() {
-  // read data from parent component that is passed from Outlet
-  // const boards = useOutletContext();
-
-  const { columns } = useLoaderData();
-
-  // // get board name from params
-  // const { boardId } = useParams();
-
-  // // find the board that has the same name as the boardId from params
-  // const [board] = boards.filter(
-  //   (board) => board.name === removeDelimiter(boardId, "-")
-  // );
+  const { columns, tasks } = useLoaderData();
 
   return (
-    <Flex as="main" className="w-full px-6 pt-6 gap-6 overflow-auto">
-      {columns.map((column) => (
-        <Column
-          key={column.name}
-          columnName={column.name}
-          tasks={column.tasks}
-        />
-      ))}
-      <AddColumnButton />
-      <Outlet />
-      {/* <CallToAction /> */}
-    </Flex>
+    <>
+      {columns ? (
+        <Flex as="main" className="w-full px-6 pt-6 gap-6 overflow-auto">
+          {columns.map((column) => (
+            <Column
+              key={column.name}
+              columnName={column.name}
+              tasks={tasks[column.name]}
+            />
+          ))}
+          <AddColumnButton />
+          <Outlet />
+        </Flex>
+      ) : (
+        <CallToAction />
+      )}
+    </>
   );
 }
