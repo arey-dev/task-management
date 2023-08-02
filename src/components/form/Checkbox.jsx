ac65475709ea3bb269852e64fe4eb0e18c110382
R.A Borja <raborja.dev@gmail.com>
2023-08-03 04:07:09 +0800

    set state to change styles when toggling

--- src/components/form/Checkbox.jsx
+++ src/components/form/Checkbox.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/prop-types */
-import { useId } from "react";
+import { useId, useState } from "react";
 import { twJoin } from "tailwind-merge";
+import { useFormContext } from "react-hook-form";
 
-export function Checkbox({ checked, label, className, ...props }) {
+export function Checkbox({ checked, name, label, className, ...props }) {
   const id = useId();
+  const { register } = useFormContext();
+  const [check, setCheck] = useState(checked);
 
   return (
     <div
@@ -13,19 +16,19 @@ export function Checkbox({ checked, label, className, ...props }) {
       )}
     >
       <input
+        onClick={() => setCheck(!check)}
         defaultChecked={checked}
         id={id}
         type="checkbox"
-        value=""
-        name="bordered-checkbox"
         className="w-4 h-4 text-primary bg-light-surface rounded-sm focus:ring-primary focus:outline-none focus:ring-1"
+        {...register(name)}
         {...props}
       />
       <label
         htmlFor={id}
         className={twJoin(
           "w-full py-4 text-body-md",
-          checked && "text-on-background line-through"
+          check && "text-on-background line-through"
         )}
       >
         {label}
