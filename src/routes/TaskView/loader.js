586241e0eb90cd84f4925bb73f34557dc257a8fd
R.A Borja <raborja.dev@gmail.com>
2023-08-03 04:07:40 +0800

    get task from db

--- src/routes/TaskView/loader.js
+++ src/routes/TaskView/loader.js
@@ -1,4 +1,19 @@
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 
-export async function loader() {
-  return null;
+export async function loader({ params }) {
+  // the task we to get
+  const taskId = params.taskId;
+
+  const boardId = params.boardId;
+
+  // reference to the task
+  const taskRef = doc(db, `boards/${boardId}/tasks`, taskId);
+
+  // query for Task
+  const taskSnap = await getDoc(taskRef);
+
+  const task = taskSnap.data();
+
+  return { task };
 }
