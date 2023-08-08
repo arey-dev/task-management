04fc9eb5777aa999dc8fd65d4abb58e5f48bed56
R.A Borja <raborja.dev@gmail.com>
2023-08-08 18:32:27 +0800

    query for id and then update

--- src/routes/EditBoard/action.js
+++ src/routes/EditBoard/action.js
@@ -1,19 +1,56 @@
-import { doc, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { db } from "../../firebase";
 import { redirect } from "react-router-dom";
+import { hypenateString, removeDelimiter } from "../../utilities";
+
+function transformFormData(obj) {
+  // transform object
+  const columns = [];
+  for (const key in obj) {
+    if (key.includes("column")) {
+      columns.push({ name: obj[key] });
+    }
+  }
+
+  const data = {
+    name: obj.name,
+    columns: columns,
+  };
+
+  return data;
+}
 
 export async function action({ request, params }) {
+  const boardName = removeDelimiter(params.boardId, "-");
+
   // Get data from AddBoard Form
   const formData = await request.formData();
 
   // create object from FormData
   const board = Object.fromEntries(formData);
 
-  // id for firestore document
-  const id = params.boardId;
+  // get id of the board that will be updated
+  let boardId;
+  const qBoard = query(
+    collection(db, "boards"),
+    where("name", "==", boardName)
+  );
+  const boardSnap = await getDocs(qBoard);
+  boardSnap.forEach((doc) => (boardId = doc.id));
 
-  // add doc to firestore
-  await updateDoc(doc(db, "boards", id), board);
+  const updatedBoard = transformFormData(board);
 
-  return redirect(`/board/${params.boardId}`);
+  // update board on firestore
+  await updateDoc(doc(db, "boards", boardId), updatedBoard);
+
+  // use board.name instead of params.boardId to redirect
+  // updated board path, if board name is changed
+  return redirect(`/board/${hypenateString(board.name)}`);
 }
