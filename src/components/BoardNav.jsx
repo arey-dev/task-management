5ac98364d0e1c3f062710a65bcd889ee10c382b5
R.A Borja <raborja.dev@gmail.com>
2023-08-10 15:28:39 +0800

    conditionally go to a route if :boardId is in params

--- src/components/BoardNav.jsx
+++ src/components/BoardNav.jsx
@@ -3,8 +3,10 @@ import { Link } from "./ui";
 import { ThemeSwitch } from "./ThemeSwitch";
 import { HideButton } from "./HideButton";
 import { hypenateString } from "../utilities";
+import { useParams } from "react-router-dom";
 
 export function BoardNav({ boards, onHideSidebar }) {
+  const params = useParams();
   return (
     <nav className="min-w-[18.75rem] pt-4 pb-8 border-e border-light-lines bg-light-surface flex flex-col relative z-20 overflow-hidden">
       <h2 className="text-heading-sm text-on-background ml-8 mb-[1.125rem]">
@@ -21,7 +23,11 @@ export function BoardNav({ boards, onHideSidebar }) {
         })}
         <li className="relative right-6">
           <Link
-            to="board/add-board"
+            to={
+              params.boardId
+                ? `board/${params.boardId}/add-board`
+                : "board/add-board"
+            }
             title="+ Create New Board"
             className="text-primary"
           />
