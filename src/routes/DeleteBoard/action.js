bafbad28fd07e27280cb8090dfe69e11ce5ba924
R.A Borja <raborja.dev@gmail.com>
2023-08-10 16:11:22 +0800

    delete board and task based on generated id

--- src/routes/DeleteBoard/action.js
+++ src/routes/DeleteBoard/action.js
@@ -1,9 +1,28 @@
-import { doc, deleteDoc } from "firebase/firestore";
+import {
+  doc,
+  deleteDoc,
+  query,
+  collection,
+  where,
+  getDocs,
+} from "firebase/firestore";
 import { db } from "../../firebase";
 import { redirect } from "react-router-dom";
+import { removeDelimiter } from "../../utilities";
 
 export async function action({ params }) {
-  const boardId = params.boardId;
+  const boardName = removeDelimiter(params.boardId, "-");
+
+  console.log(boardName);
+
+  let boardId;
+  const qBoard = query(
+    collection(db, "boards"),
+    where("name", "==", boardName)
+  );
+  const boardSnap = await getDocs(qBoard);
+  boardSnap.forEach((doc) => (boardId = doc.id));
+  console.log(boardId);
 
   await deleteDoc(doc(db, "boards", boardId));
 
--- src/routes/DeleteTask/action.js
+++ src/routes/DeleteTask/action.js
@@ -1,10 +1,37 @@
-import { doc, deleteDoc } from "firebase/firestore";
+import {
+  doc,
+  deleteDoc,
+  query,
+  collection,
+  where,
+  getDocs,
+} from "firebase/firestore";
 import { db } from "../../firebase";
 import { redirect } from "react-router-dom";
+import { removeDelimiter } from "../../utilities";
 
 export async function action({ params }) {
-  const boardId = params.boardId;
-  const taskId = params.taskId;
+  const boardName = removeDelimiter(params.boardId, "-");
+
+  const taskName = removeDelimiter(params.taskId, "-");
+
+  // query for boards in the params
+  let boardId;
+  const qBoard = query(
+    collection(db, "boards"),
+    where("name", "==", boardName)
+  );
+  const boardSnap = await getDocs(qBoard);
+  boardSnap.forEach((doc) => (boardId = doc.id));
+
+  // query for task in the params
+  let taskId;
+  const qTask = query(
+    collection(db, `boards/${boardId}/tasks`),
+    where("title", "==", taskName)
+  );
+  const taskSnap = await getDocs(qTask);
+  taskSnap.forEach((doc) => (taskId = doc.id));
 
   await deleteDoc(doc(db, `boards/${boardId}/tasks`, taskId));
 
