bcc3a0c89f4c80dfb9e0deb5a0fc217ba42cbec1
R.A Borja <raborja.dev@gmail.com>
2023-08-14 18:29:29 +0800

    improve readability

--- src/routes/DeleteBoard/action.js
+++ src/routes/DeleteBoard/action.js
@@ -11,20 +11,43 @@ import { redirect } from "react-router-dom";
 import { removeDelimiter } from "../../utilities";
 
 export async function action({ params }) {
+  // Extract board name from params
   const boardName = removeDelimiter(params.boardId, "-");
 
-  console.log(boardName);
+  // Find the board in the database
+  const boardId = await findBoardId(boardName);
 
-  let boardId;
+  // If board not found, log a message and return
+  if (!boardId) {
+    console.log("Board not found");
+    return;
+  }
+
+  // Delete the board
+  await deleteBoard(boardId);
+
+  // Redirect to the homepage
+  return redirect("/");
+}
+
+// Function to find board ID
+async function findBoardId(boardName) {
   const qBoard = query(
     collection(db, "boards"),
     where("name", "==", boardName)
   );
   const boardSnap = await getDocs(qBoard);
-  boardSnap.forEach((doc) => (boardId = doc.id));
-  console.log(boardId);
 
+  // If board not found, return null
+  if (boardSnap.empty) {
+    return null;
+  }
+
+  // Extract and return the board ID
+  return boardSnap.docs[0].id;
+}
+
+// Function to delete a board
+async function deleteBoard(boardId) {
   await deleteDoc(doc(db, "boards", boardId));
-
-  return redirect("/");
 }
