13c4974139d70d106332eb60e3b121bfbcece668
R.A Borja <raborja.dev@gmail.com>
2023-08-16 21:43:02 +0800

    show subtask count if there is subtask

--- src/components/board/Task.jsx
+++ src/components/board/Task.jsx
@@ -3,27 +3,34 @@ import { useNavigate, useParams } from "react-router-dom";
 import { hypenateString } from "../../utilities";
 
 export function Task({ task }) {
-  const { title, subtasks } = task;
-
-  const subtaskCompleted = Object.values(subtasks).filter((value) => value).length;
-
-  const subtaskCount = Object.keys(subtasks).length;
-
   const navigate = useNavigate();
 
   const { boardId } = useParams();
 
+  const { title, subtasks } = task;
+
+  const subtaskCompleted = Object.values(subtasks).filter(
+    (value) => value
+  ).length;
+
+  const subtaskCount = Object.keys(subtasks).length;
+
   return (
     <section
       onClick={() =>
-        navigate(`/board/${boardId}/task/${hypenateString(title)}`, {state: task})
+        navigate(`/board/${boardId}/task/${hypenateString(title)}`, {
+          state: task,
+        })
       }
       className="bg-light-surface rounded-lg task-shadow px-4 py-6 last-of-type:last:mb-12 cursor-pointer text-light-on-surface hover:text-primary"
     >
       <h3 className="text-heading-md mb-2">{title}</h3>
-      <p className="text-body-md text-on-background">
-        <span>{subtaskCompleted}</span> of <span>{subtaskCount}</span> subtasks
-      </p>
+      {subtaskCount > 0 && (
+        <p className="text-body-md text-on-background">
+          <span>{subtaskCompleted}</span> of <span>{subtaskCount}</span>{" "}
+          subtasks
+        </p>
+      )}
     </section>
   );
 }
