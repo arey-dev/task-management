18fac9befec0e4eedf21f65fc7f56dfb90e57479
R.A Borja <raborja.dev@gmail.com>
2023-08-16 19:34:26 +0800

    center text

--- src/components/board/AddColumnButton.jsx
+++ src/components/board/AddColumnButton.jsx
@@ -5,7 +5,7 @@ export function AddColumnButton() {
 
   return (
     <Link
-      className="w-[17.5rem] max-h-[53.3125rem] relative top-[2.45rem] bg-add-column text-on-background text-2xl rounded-md shrink-0 hover:text-primary transition z-40"
+      className="w-[17.5rem] max-h-[53.3125rem] relative top-[2.45rem] flex justify-center  items-center bg-add-column text-on-background text-2xl rounded-md shrink-0 hover:text-primary transition z-40"
       to={`/board/${boardId}/add-column`}
     >
       + New Column
