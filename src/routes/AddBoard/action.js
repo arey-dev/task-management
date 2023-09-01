5cc3e1464c266c5ba28257f42c28da32e81b0a6c
R.A Borja <raborja.dev@gmail.com>
2023-09-01 13:28:37 +0800

    add boards collection to logged in user

--- src/routes/AddBoard/action.js
+++ src/routes/AddBoard/action.js
@@ -1,13 +1,17 @@
 import { addDoc, collection } from "firebase/firestore";
-import { db } from "../../firebase";
+import { auth, db } from "../../firebase";
 import { redirect } from "react-router-dom";
 
 export async function action({ request }) {
+  // current user
+  const user = auth.currentUser;
+
   // Get data from AddBoard Form
   const board = await request.json();
 
-  // add doc to firestore
-  await addDoc(collection(db, "boards"), board);
+  if (user) {
+    await addDoc(collection(db, `users/${user.uid}/boards`), board);
+  }
 
   return redirect("/");
 }
