9f7cf6020fb4c02958cbffa304682453922bb539
R.A Borja <raborja.dev@gmail.com>
2023-09-01 13:33:59 +0800

    return boards from a specific user

--- src/routes/Root/loader.js
+++ src/routes/Root/loader.js
@@ -1,24 +1,29 @@
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../firebase";
+import { auth, db } from "../../firebase";
 
 export async function loader() {
+  // current user
+  const user = auth.currentUser;
+
   // Initialize an array to store board data
   const boardList = [];
 
-  // Get a snapshot of all boards from the database
-  const querySnapshot = await getAllBoards();
+  if (user) {
+    // Get a snapshot of all boards from the database
+    const querySnapshot = await getAllBoards(user.uid);
 
-  // Populate the boardList array with data from each board
-  querySnapshot.forEach((doc) => {
-    boardList.push(doc.data());
-  });
+    // Populate the boardList array with data from each board
+    querySnapshot.forEach((doc) => {
+      boardList.push(doc.data());
+    });
+  }
 
   // Return the array of board data
   return { boardList };
 }
 
 // Function to get a snapshot of all boards from the database
-async function getAllBoards() {
-  const querySnapshot = await getDocs(collection(db, "boards"));
+async function getAllBoards(uid) {
+  const querySnapshot = await getDocs(collection(db, `users/${uid}/boards`));
   return querySnapshot;
 }
