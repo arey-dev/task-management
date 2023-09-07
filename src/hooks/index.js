22354dbeb146cfd0f32e2afd30faf95fd62b057a
R.A Borja <raborja.dev@gmail.com>
2023-09-07 17:34:55 +0800

    remove uselocalstorage from entry file

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,5 @@
 import { useScreenSize } from "./useScreenSize";
-import { useDarkMode, useLocalStorage } from "./useDarkMode";
+import { useDarkMode } from "./useDarkMode";
 import { useAuthState } from "./useAuthState";
 
-export { useScreenSize, useDarkMode, useLocalStorage, useAuthState };
+export { useScreenSize, useDarkMode, useAuthState };
