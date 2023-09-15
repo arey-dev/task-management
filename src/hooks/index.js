df7a2334d88ea174a37d8a2e035ce7c0260a1a81
R.A Borja <raborja.dev@gmail.com>
2023-09-16 00:09:56 +0800

    add useBoardData

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
 import { useScreenSize } from "./useScreenSize";
 import { useDarkMode } from "./useDarkMode";
 import { useAuthState } from "./useAuthState";
+import { useBoardData } from "./useBoardData";
 
-export { useScreenSize, useDarkMode, useAuthState };
+export { useScreenSize, useDarkMode, useAuthState, useBoardData };
